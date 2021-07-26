f063e490f980e9f124539a924c142a7a53971ac3
youngkwon02 <jakealden97@gmail.com>
2021-07-26 15:23:58 +0900

    feat : account-create User class

--- src/DoMain/account/models.py
+++ src/DoMain/account/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class User_info(models.Model):
+  user_seq = models.AutoField(primary_key=True)
+  user_email = models.EmailField(max_length=254, verbose_name="user_email", blank=False)
+  user_password = models.CharField(max_length=512, verbose_name="user_passwd")
+  register_dttm = models.DateField(auto_now_add=True, verbose_name="Register_date")
+  is_active = models.BooleanField(default=False)
+  
+  def __str__(self):
+      return self.user_email
