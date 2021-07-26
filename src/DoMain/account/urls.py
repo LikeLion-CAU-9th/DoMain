96a5e78f6a05ce0564c8f2b4dcca99a34cc9be12
youngkwon02 <jakealden97@gmail.com>
2021-07-26 15:19:17 +0900

    feat : add url patterns of login and join

--- src/DoMain/account/urls.py
+++ src/DoMain/account/urls.py
@@ -4,6 +4,9 @@ from django.urls import path
 from .views import Activate
 
 urlpatterns = [
-  path('login/', views.page_login, name="login"),
+  path('login/', views.login_view, name="login_view"),
+  path('loginAction/', views.login_action, name="login_action"),
+  path('join/', views.join_view, name="join_view"),
+  path('joinAction/', views.join_action, name="join_action"),
   path('activate/<str:uidb64>/<str:token>', Activate.as_view(), name="activate"),
 ]
