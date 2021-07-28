a52ce2475f80e042da109c946cf31a0b7984ff89
youngkwon02 <jakealden97@gmail.com>
2021-07-28 14:34:05 +0900

    feat : update settings

--- src/DoMain/DoMain/settings.py
+++ src/DoMain/DoMain/settings.py
@@ -34,7 +34,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'account'
+    'account',
+    'widget',
 ]
 
 MIDDLEWARE = [
