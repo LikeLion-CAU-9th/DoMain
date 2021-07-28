f9cf9ba665b5c002c23fa5ca4fc344af7145cf40
youngkwon02 <jakealden97@gmail.com>
2021-07-28 17:16:34 +0900

    refactor : crawl_finance modify target URL

--- src/DoMain/widget/finance.py
+++ src/DoMain/widget/finance.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
-URL='https://finance.naver.com/'
+URL='https://www.naver.com'
 
 def crawl_finance(stockItem):
   options = webdriver.ChromeOptions()
@@ -10,14 +10,15 @@ def crawl_finance(stockItem):
   driver.maximize_window()
   driver.implicitly_wait(7)
   driver.get(URL)
-  search = driver.find_element_by_id('stock_items')
+  search = driver.find_element_by_id('query')
   search.send_keys(stockItem)
-  driver.find_elements_by_class_name('snb_search_btn')[0].click()
-  # price = driver.find_element_by_css_selector('.tbl_search tbody tr td:nth-child(2)').text
+  driver.find_elements_by_css_selector('#search_btn')[0].click()
+  
+  price = "NoN"
   attempt = 0
-  while(attempt < 3):
+  while(attempt < 2):
     try:
-      price = driver.find_element_by_css_selector('.rate_info .today .no_today').text
+      price = driver.find_element_by_css_selector('.spt_con strong').text
     except:
       pass
     attempt += 1
