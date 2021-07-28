4d1ec88ccd19a31b35b052404dfad1be847dfa4a
oereo <dlstpgns0406@gmail.com>
2021-07-28 14:33:10 +0900

    feat: add backgroundEffect in landing

--- src/DoMain/store/static/js/landingBackground.js
+++ src/DoMain/store/static/js/landingBackground.js
@@ -2,10 +2,10 @@ const html = document.documentElement;
 const canvas = document.getElementById("apple");
 const context = canvas.getContext("2d");
 
-const frameCount = 196;
+const frameCount = 28;
 
 const currentFrame = index =>
-  `../static/image/landingBackgroundScene1/scene${index
+  `../static/image/landingBackgroundScene2/scene${index
     .toString()
     .padStart(5, "0")}.png`;
 console.log(currentFrame);
