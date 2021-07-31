7f1c2add33b979e6f71dc17630f863937f5c978f
yunseonyeong <64634970+yunseonyeong@users.noreply.github.com>
2021-07-31 15:57:26 +0900

    feat : add sticky note widget data

--- src/DoMain/widget/static/utils/widget.js
+++ src/DoMain/widget/static/utils/widget.js
@@ -3,6 +3,13 @@ const widgetJSON = [
     "type": "finance",
     "posX": "400px",
     "posY": "300px",
-    "content": {"items": ["삼성전자", "네이버", "카카오", "JYP Ent"]}
+    "contents": {"items": ["삼성전자", "네이버", "카카오", "JYP Ent"]}
+  },
+  {
+    "type": "stickynote",
+    "posX": "400px",
+    "posY": "300px",
+    "contents": {"title": "다이어트 계획", "content": "없음"}
   }
 ]
+
