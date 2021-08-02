e7b4c743e63c288b6f162674d836420335bbb054
oereo <dlstpgns0406@gmail.com>
2021-08-02 13:08:25 +0900

    feat: base.html settings

--- src/DoMain/DoMain/settings.py
+++ src/DoMain/DoMain/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'DoMain.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'Domain/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
