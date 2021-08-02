0e6e46f7f06b99587a6b32328aabc52b2b4db935
youngkwon02 <jakealden97@gmail.com>
2021-08-02 23:26:28 +0900

    feat : add Layout model in the widget app

--- src/DoMain/widget/models.py
+++ src/DoMain/widget/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from account.models import User_info
 
-# Create your models here.
+class Layout(models.Model):
+  layout_seq = models.AutoField(primary_key=True)
+  owner = models.ForeignKey(User_info, related_name="layout", on_delete=models.CASCADE, db_column="owner")
+  data = models.TextField(null=False, default="[]")
+  first_creater = models.ForeignKey(User_info, related_name="layout", on_delete=models.CASCADE, db_column="first_creater")
+  create_date = models.DateField(auto_now_add=True, verbose_name="create_date")
+  is_applied = models.BooleanField(null=False, default=False)
+  from_store = models.BooleanField(null=False, default=False)
+  
