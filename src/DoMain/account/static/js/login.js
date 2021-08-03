6a39ab87caed5ec7eb07acab7a9a1cc1e0c88249
Seojisoo20191941 <amy0480@naver.com>
2021-08-03 15:45:38 +0900

    feat:fix reflect review

--- src/DoMain/account/static/js/login.js
+++ src/DoMain/account/static/js/login.js
@@ -1,42 +1,36 @@
+function changeBox1() {
+  let wgi = document.getElementById("white-sign-in");
+  let gsu = document.getElementById("green-sign-up");
+  let whitebox = document.getElementById("white-box");
+  let greenbox = document.getElementById("green-box");
+  let wgu = document.getElementById("white-sign-up");
+  let gsi = document.getElementById("green-sign-in");
 
-    function changeBox1() {
-      console.log("클릭");
-      let wgi = document.getElementById("white-sign-in");
-      let gsu = document.getElementById("green-sign-up");
-      let whitebox = document.getElementById("white-box");
-      let greenbox = document.getElementById("green-box");
-      let wgu = document.getElementById("white-sign-up");
-      let gsi = document.getElementById("green-sign-in");
+  wgi.style.display = "none";
+  gsu.style.display = "none";
+  whitebox.style.float = "right";
+  greenbox.style.marginLeft = "0%";
 
-      wgi.style.display = "none";
-      gsu.style.display = "none";
-      whitebox.style.float = "right";
-      greenbox.style.marginLeft = "0%";
+  wgu.style.display = "block";
+  gsi.style.display = "block";
 
-      if(greenbox.style.marginLeft ==="0%"){
-        wgu.style.display = "block";
-        gsi.style.display = "block";
-      }
-    }
+}
 
-    function changeBox2() {
-      console.log("클릭");
-      let whitebox = document.getElementById("white-box");
-      let greenbox = document.getElementById("green-box");
-      let wgu = document.getElementById("white-sign-up");
-      let gsi = document.getElementById("green-sign-in");
-      let wgi = document.getElementById("white-sign-in");
-      let gsu = document.getElementById("green-sign-up");
+function changeBox2() {
+  let wgi = document.getElementById("white-sign-in");
+  let gsu = document.getElementById("green-sign-up");
+  let whitebox = document.getElementById("white-box");
+  let greenbox = document.getElementById("green-box");
+  let wgu = document.getElementById("white-sign-up");
+  let gsi = document.getElementById("green-sign-in");
 
-      wgu.style.display = "none";
-      gsi.style.display = "none";
-      whitebox.style.float = "left";
+  wgu.style.display = "none";
+  gsi.style.display = "none";
+  whitebox.style.float = "left";
 
-      greenbox.style.marginLeft = "64.8%";
-      if(greenbox.style.marginLeft === "64.8%"){
-        wgi.style.display = "block";
-        gsu.style.display = "block";
-      }
+  greenbox.style.marginLeft = "64.8%";
 
-     
-  }
+  wgi.style.display = "block";
+  gsu.style.display = "block";
+
+}
