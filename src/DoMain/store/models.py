01433f33ea49526e9dc1e760d30d68ce519b1c87
oereo <dlstpgns0406@gmail.com>
2021-08-04 17:17:28 +0900

    feat: add WidgetType class for Widget model class

--- src/DoMain/store/models.py
+++ src/DoMain/store/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from account.models import User
 
-# Create your models here.
+
+class WidgetType:
+    """
+    WigetType
+    - simpleWidget, LayoutWidget
+    """
+    SIMPLE_WIDGET = 'simple_widget'
+    LAYOUT_WIDGET = 'layout_widget'
+
+    WIDGET_TYPES = [
+        (SIMPLE_WIDGET, '단일 위젯'),
+        (LAYOUT_WIDGET, '레이아웃 위젯')
+    ]
+
+
+class Widget(models.Model):
+    user = models.ForeignKey()
+
+
+
+
+
+
+
