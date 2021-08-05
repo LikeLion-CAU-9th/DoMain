5049829886f3c00dd7c030bb52a42fc03fe8dbc9
MyeongjunKim <ilikerunning@naver.com>
2021-08-05 22:07:58 +0900

    refactor: dday.js and dday.html

--- src/DoMain/widget/static/dday/dday.js
+++ src/DoMain/widget/static/dday/dday.js
@@ -8,34 +8,34 @@ const ddayListup = (ITEMS) => {
     let stDate = new Date(year, month, day);
 
     let ddayList=[];
-    for(let i = 0; i < ITEMS.length; i++) {
+    for(let i = 0; i < ITEMS.length; i++){
         ddayName = ITEMS[i].split(':')[0];
         ddayDateArr = ITEMS[i].split(':')[1].split('-');
         endDate = new Date(ddayDateArr[0], ddayDateArr[1], ddayDateArr[2]);
         let dday = (endDate.getTime() - stDate.getTime())/(1000*60*60*24);
         ddayList.push([ddayName, dday]);
-    }
-    ddayName = document.querySelector('#ddayName');
-    ddayName.innerHTML = ddayList[0][0];
+    };
 
-    dday = document.querySelector('#dday');
-    dday.innerHTML = "D -" + ddayList[0][1];
+    let contents = "";
+    for(let i = 0; i < ddayList.length; i++){
+        contents = contents + "<hr><p>"+ddayList[i][0]+"</p><p>"+"D - "+ddayList[i][1].toString()+"</p>"
+    };
+    console.log(contents);
+    const ddayAdd = document.querySelector('#ddayAdd');
+    ddayAdd.innerHTML = contents+"<hr>";
+
 }
 
 const ddayListHide = () => {
-    document.querySelector('#ddayName').style.display = 'none';
-    document.querySelector('#dday').style.display = 'none';
+    document.querySelector('#ddayAdd').style.display = 'none';
 }
 
 const ddayListShow = () => {
-    document.querySelector('#ddayName').style.display = 'block';
-    document.querySelector('#dday').style.display = 'block';
+    document.querySelector('#ddayAdd').style.display = 'block';
 }
 
 window.onload = () => {
     const ITEMS = widgetJSON[1]['content']['items'];
-    // ITEMS = ["헤커톤:2021-08-15", "개강:2021-09-01", "한살 더먹음:2022-01-01"]
-    
     document.querySelector(".title").addEventListener('load', ddayListup(ITEMS), false);
     document.querySelector(".title").addEventListener('load', ddayListHide(), false);    
    }
