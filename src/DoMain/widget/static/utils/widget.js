0a0c304139877edfc4333a280286640d3747a576
youngkwon02 <jakealden97@gmail.com>
2021-08-05 21:28:23 +0900

    feat : searching widget

--- src/DoMain/widget/static/utils/widget.js
+++ src/DoMain/widget/static/utils/widget.js
@@ -4,5 +4,11 @@ const widgetJSON = [
     "posX": "400px",
     "posY": "300px",
     "content": {"items": ["삼성전자", "네이버", "카카오", "JYP Ent"]}
+  },
+  {
+    "type": "searching",
+    "posX": "500px",
+    "posY": "300px",
+    "content": {"engine": "naver", "width": "500px", "height": "70px"}
   }
 ]
