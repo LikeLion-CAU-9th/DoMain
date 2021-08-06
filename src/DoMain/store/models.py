ea1ecc032c30c436e665e51b5e33644a464933f2
oereo <dlstpgns0406@gmail.com>
2021-08-06 21:13:57 +0900

    refactor: fix StoreWidget migration format

--- src/DoMain/store/models.py
+++ src/DoMain/store/models.py
@@ -31,7 +31,7 @@ class WidgetType:
 
 
 class StoreWidget(AbstractBaseWidget):
-    download = models.IntegerField()
+    download = models.IntegerField(default=0)
     widget_type = models.CharField(
         max_length=20, 
         choices=WidgetType.WIDGET_TYPES,
@@ -42,6 +42,7 @@ class StoreWidget(AbstractBaseWidget):
     name = models.CharField(max_length=63, null=True, blank=True, help_text='위젯 제목')
     is_removed = models.BooleanField(default=False)
     like_users = models.ManyToManyField(User_info, related_name="like_widgets", blank=True)
+    score = models.IntegerField(default=0)
     # image = models.ImageField()
 
 
@@ -56,11 +57,19 @@ class StoreWidget(AbstractBaseWidget):
     def host_id(self):
         return self.user.id
 
-    # @peroperty
+    # @property
     # def score(self):
     #     score = WidgetStarredUser.objects.filter(is_removed=False).aggregate(Avg('score'))
     #     return score
 
+    @property
+    def star_grade(self):
+        star_grade = Comment.objects.all().aggregate(Avg(star_grade))
+        return star_grade
+
+    def score_earned(self, point):
+        return self.score + point
+        
 
 # class WidgetGrade(models.Model):
 #     user = models.ForeignKey(User)
@@ -73,5 +82,3 @@ class StoreWidget(AbstractBaseWidget):
 
 # class WidgetStarredUser(WidgetGrade):
 #     score = models.FloatField()
-
-
