70ccf30b78dd31b6782876d461ed481c43ed8599
yunseonyeong <64634970+yunseonyeong@users.noreply.github.com>
2021-08-06 13:56:12 +0900

    feat : connect stickynote urls

--- src/DoMain/widget/urls.py
+++ src/DoMain/widget/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 
 urlpatterns = [
   path('finance/', views.finance_view, name="finance"),
-  path('getFinance/', views.get_finance, name="get-finance")
+  path('getFinance/', views.get_finance, name="get-finance"),
+  path('stickynote/', views.stickynote_view, name="stickynote"),
+  
 ]
