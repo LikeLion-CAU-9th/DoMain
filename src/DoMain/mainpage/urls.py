00f1378a66e14aae05123475781cf1e1ef31df4f
youngkwon02 <jakealden97@gmail.com>
2021-08-07 15:49:19 +0900

    feat : add test url

--- src/DoMain/mainpage/urls.py
+++ src/DoMain/mainpage/urls.py
@@ -1,10 +1,10 @@
 from os import name
 from mainpage.views import main
 from django.urls import path
-
+from . import views
 
 urlpatterns = [
     path('', main, name="main"),
     path('start/', main, name="finance"),
+    path('test/', views.test_view, name="test"),
 ]
-
