df54b1cdbd3c6b72e5ab62e07a145877a4640c9b
yunseonyeong <64634970+yunseonyeong@users.noreply.github.com>
2021-08-07 18:27:15 +0900

    feat : create test func for test layout edit

--- src/DoMain/mainpage/views.py
+++ src/DoMain/mainpage/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from widget.views import insert_dummy_layout, view_list
 
-# Create your views here.
 def main(request):
     return render(request, 'mainpage.html')
+
+def test(request):
+    layout_list = view_list(request)
+    data = {'layout_list': layout_list}
+    return render(request, 'test.html', data)
