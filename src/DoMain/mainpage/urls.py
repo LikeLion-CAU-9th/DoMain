c30c9742284667915ca6f14fb482a162c5a01f6d
yunseonyeong <64634970+yunseonyeong@users.noreply.github.com>
2021-08-07 18:27:52 +0900

    feat : connect test urls

--- src/DoMain/mainpage/urls.py
+++ src/DoMain/mainpage/urls.py
@@ -1,10 +1,11 @@
 from os import name
-from mainpage.views import main
+from . import views
 from django.urls import path
 
 
 urlpatterns = [
-    path('', main, name="main"),
-    path('start/', main, name="finance"),
+    path('', views.main, name="main"),
+    path('test/', views.test, name="test"),
+    path('start/', views.main, name="finance"),
 ]
 
