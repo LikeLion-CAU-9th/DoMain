624a6f8b26137447fedc8c2e95c44a7bc8c9dbc1
youngkwon02 <jakealden97@gmail.com>
2021-08-08 01:43:05 +0900

    refactor : create LAYOUT variable

--- src/DoMain/widget/views.py
+++ src/DoMain/widget/views.py
@@ -74,10 +74,11 @@ def is_distinct_QS(QS):
   return False
 
 
+LAYOUT = '[{"type": "finance","contents": {"width": "340px", "posX": "300px","posY": "500px","items": ["삼성전자", "네이버", "카카오", "JYP Ent"]}},{"type": "stickynote","contents": {"width": "160px", "height": "160px", "posX": "700px","posY": "500px", "title": "Sticky Note", "memo": "내용을 작성하세요!"}},{"type": "searching","contents": {"width": "500px", "height": "70px", "posX": "100px","posY": "100px", "bgColor": "#ee531e", "engine": "google"}}]'
+
+
 def insert_dummy_layout(request, apply):
   user = get_user_inst(request)
-  Layout.objects.create(owner=user, creater=user, from_store=False, is_applied=apply,data='[{"type": "finance","contents": {"width": "340px", "posX": "300px","posY": "500px","items": ["삼성전자", "네이버", "카카오", "JYP Ent"]}},{"type": "stickynote","contents": {"width": "160px", "height": "160px", "posX": "700px","posY": "200px", "title": "Sticky Note", "memo": "내용을 작성하세요!"}}]')
+  Layout.objects.create(owner=user, creater=user, from_store=False, is_applied=apply,data=LAYOUT)
   return True
 
-
-# [{"type": "finance","contents": {"width": "340px", "posX": "300px","posY": "500px","items": ["삼성전자", "네이버", "카카오", "JYP Ent"]}},{"type": "stickynote","contents": {"width": "160px", "height": "160px", "posX": "700px","posY": "500px", "title": "Sticky Note", "memo": "내용을 작성하세요!"}}]
