7d624db819577f5b6eeeee9856bbfcaedfa848c6
youngkwon02 <jakealden97@gmail.com>
2021-08-07 16:09:42 +0900

    refactor : finance widget for rendering

--- src/DoMain/widget/static/finance/finance.js
+++ src/DoMain/widget/static/finance/finance.js
@@ -1,10 +1,17 @@
-const finance = (stockItems) => {
+const finance = (contents) => {
+  let stockItems = contents.items;
+  let elem = getFinanceElements();
+  document.querySelector('.main-board').innerHTML = elem;
+  document.querySelector('.financeBoard').style.width = contents.width;
+  document.querySelector('.financeBoard').style.left = contents.posX;
+  document.querySelector('.financeBoard').style.top = contents.posY;
   const URL = "/widget/getFinance/";
   let obj = {};
   for(let i = 0; i < stockItems.length; i++) {
     obj[i] = stockItems[i];
   }
   priceStr = AjaxCall(URL, data=obj);
+  renderName(stockItems);
   renderPrice(priceStr);
 }
 
