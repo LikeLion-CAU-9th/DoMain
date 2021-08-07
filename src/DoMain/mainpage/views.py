f842f858bc53d4f44b06a5f4a79ded717337b3a4
youngkwon02 <jakealden97@gmail.com>
2021-08-07 15:49:32 +0900

    feat : add test_view function

--- src/DoMain/mainpage/views.py
+++ src/DoMain/mainpage/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def main(request):
     return render(request, 'mainpage.html')
+
+def test_view(request):
+    return render(request, 'test.html')
