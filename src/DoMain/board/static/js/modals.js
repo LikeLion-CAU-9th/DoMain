e7e45a76e62929622bd924e255ce1476d92b31aa
ohjeeyoung <jeeyoung990725@naver.com>
2021-08-08 11:36:25 +0900

    feat:add vertical tab

---
+++ src/DoMain/board/static/js/modals.js
@@ -0,0 +1,23 @@
+const showMenu = (toggleId, navbarId, bodyId) => {
+    const toggle = document.getElementById(toggleId),
+    navbar = document.getElementById(navbarId),
+    bodypadding = document.getElementById(bodyId)
+
+    if( toggle && navbar ) {
+        toggle.addEventListener('click', ()=>{
+            navbar.classList.toggle('expander');
+
+            bodypadding.classList.toggle('body-pd')
+        })
+    }
+}
+
+showMenu('nav-toggle', 'navbar', 'body-pd')
+
+/* LINK ACTIVE */
+const linkColor = document.querySelectorAll('.nav__link')
+function colorLink() {
+    linkColor.forEach(l=> l.classList.remove('active'))
+    this.classList.add('active')
+}
+linkColor.forEach(l=> l.addEventListener('click', colorLink))
