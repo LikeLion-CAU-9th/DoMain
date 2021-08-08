58e75f381253cde57c394546321b990d3f2209fa
MyeongjunKim <ilikerunning@naver.com>
2021-08-09 01:19:58 +0900

    refactor: join function completion

--- src/DoMain/account/static/js/join.js
+++ src/DoMain/account/static/js/join.js
@@ -3,7 +3,7 @@ const emailCheck = () => {
     const userEmail = document.querySelector('#user_email').value;
     const mailFormat = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     const mailValidation = mailFormat.test(userEmail.toLowerCase());
-    
+    console.log("메일 유효", mailValidation)
     // 디비 조회 중복 검사
     let mailOverap = false;
     $.ajax({
@@ -11,8 +11,8 @@ const emailCheck = () => {
         type: "GET",
         data: {'email': userEmail},
         dataType : "text",
+        async: false,
         success: function(response) {
-            console.log(response)
             if(response == 'Overap') {
                 mailOverap = true;
             }
@@ -21,22 +21,21 @@ const emailCheck = () => {
             console.log("error")
         }
     })
-    console.log(mailOverap)
-    setTimeout(check = ()=>{
-        // console.log(mailValidation)
-        if(mailValidation && !mailOverap) {
-            return true;
+    console.log("메일 중복 여부", mailOverap)
+    
+    let response = false;
+    if(mailValidation && !mailOverap) {
+            response = true;
         }
-        return false;
-    },300);
+    return response
 }
-
 
 // 비밀번호 두개 일치하는지 경우
 const passwordCheck = () => {
     const userPwd = document.querySelector('#user_pwd').value;
     const userPwdCheck = document.querySelector('#user_pwd_check').value;
-    if(userPwd === userPwdCheck) {
+    if(userPwd == userPwdCheck & userPwd != "") {
+        console.log("비번 일치")
         return true;
     }
     return false;
@@ -46,22 +45,25 @@ const passwordCheck = () => {
 const bothCheck = () => {
     const joinBtn = document.querySelector('#joinBtn');
     const userName = document.querySelector('#user_name').value;
-    
-    if(emailCheck() && passwordCheck()) {
+    const emCheck = emailCheck();
+    const pwCheck = passwordCheck();
+    console.log("이메일 검사, 비밀번호 검사",emCheck, pwCheck);
+    if(emCheck && pwCheck) {
         if(userName.length != 0){
+            console.log("성공")
             joinBtn.style.pointerEvents = '';
-            joinBtn.style.background = 'royalblue';
+            joinBtn.style.background = 'rgba(106, 184, 157,1)';
             return true;
         }
     }
     joinBtn.style.pointerEvents = 'none';
-    joinBtn.style.background = 'gray';
+    joinBtn.style.background = 'darkslategray';
     return false;
 
 }
 
 
-window.onload = () => {
+const joinStart = () => {
 
     document.addEventListener('keydown', function(event) {
         if (event.keyCode === 13) {
@@ -70,8 +72,10 @@ window.onload = () => {
     }, true);
 
     const joinBtn = document.querySelector('#joinBtn');
-    joinBtn.style.pointerEvents = "none";
-    joinBtn.style.background = 'gray';
+    setTimeout(check = ()=>{
+        joinBtn.style.pointerEvents = 'none';
+        joinBtn.style.background = 'darkslategray';
+    },100);
     document.querySelector('#user_name').addEventListener('keyup', bothCheck, false);
     document.querySelector('#user_email').addEventListener('keyup', bothCheck, false);
     document.querySelector('#user_pwd_check').addEventListener('keyup', bothCheck, false);
