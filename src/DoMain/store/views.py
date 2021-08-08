4f328d679f77a0732c898a95b4523c51e94a19c4
oereo <dlstpgns0406@gmail.com>
2021-08-08 22:16:42 +0900

    refactor: fix comment.time to string for json

--- src/DoMain/store/views.py
+++ src/DoMain/store/views.py
@@ -4,6 +4,8 @@ from store.models import StoreWidget
 from store.models import Comment
 from account.models import User_info
 import json
+from django.utils import timezone
+from datetime import datetime
 
 
 def landing_page(request):
@@ -29,21 +31,27 @@ def mypage(request):
 def comment_write(request):
     email= request.session['user_email']
     user = User_info.objects.get(user_email=email)
-    print("b")
+    
     if request.method == "POST":
-        print("a")
         comment = Comment()
         comment.writer = user
         comment.widget = get_object_or_404(StoreWidget, seq=request.POST.get('widget_id'))
         comment.content = request.POST.get('body')
         comment.save()
+
+        kst = datetime.now(timezone('Asia/Seoul'))
+
         ret = {
             'body': comment.content,
-            'time': comment.time.strftime('%Y-%m-%d'),
+            'time': kst.strftime(
+        '%Y년 %m월 %d일 %H:%M %p'.encode('unicode-escape').decode()
+    ).encode().decode('unicode-escape'),
             'user': comment.writer.user_name
         }
         return HttpResponse(json.dumps(ret), content_type="application/json")
         # return redirect('/store/widget/'+str(id))
+# SSR CSR
+
 
 # def reply_comment(request, id):
 #     email = request.session['user_email']
