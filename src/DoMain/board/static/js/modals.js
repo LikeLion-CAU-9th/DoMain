a7ec4823a5cf0740527b55faf9bc8ddce4fcfeed
ohjeeyoung <jeeyoung990725@naver.com>
2021-08-08 16:25:56 +0900

    fix:vertical var

--- src/DoMain/board/static/js/modals.js
+++ src/DoMain/board/static/js/modals.js
@@ -1,3 +1,4 @@
+/* EXPANDER MENU */
 const showMenu = (toggleId, navbarId, bodyId) => {
     const toggle = document.getElementById(toggleId),
     navbar = document.getElementById(navbarId),
@@ -21,3 +22,17 @@ function colorLink() {
     this.classList.add('active')
 }
 linkColor.forEach(l=> l.addEventListener('click', colorLink))
+
+/* COLLAPSE MENU */
+const linkCollapse = document.getElementsByClassName('collapse__link')
+var i
+
+for(i=0;i<linkCollapse.length;i++) {
+    linkCollapse[i].addEventListener('click', function(){
+        const collapseMenu = this.nextElementSibling
+        collapseMenu.classList.toggle('showCollapse')
+
+        const rotate = collapseMenu.previousElementSibling
+        rotate.classList.toggle('rotate')
+    });
+}
