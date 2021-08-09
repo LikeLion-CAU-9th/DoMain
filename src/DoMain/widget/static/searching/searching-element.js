d0e37b51b862d7f28130ef4b5026220ac577f3fd
youngkwon02 <jakealden97@gmail.com>
2021-08-10 06:21:23 +0900

    feat : modify-btn styling

--- src/DoMain/widget/static/searching/searching-element.js
+++ src/DoMain/widget/static/searching/searching-element.js
@@ -1,4 +1,4 @@
 const getSearchingElements = () => {
-  elem = '<div class="widget widget-search"><input type="hidden" class="searchingHidden"><input class="searching" placeholder="Search..."></input><div class="searchBtn-container"><img src="/static/image/magnifying-glass.png"></div></div>';
+  elem = '<div class="widget widget-search"><input type="hidden" class="searchingHidden"><div class="modify-btn hide" onclick=""><img src="/static/image/modify-icon.png"></div><input class="searching" placeholder="Search..."></input><div class="searchBtn-container"><img src="/static/image/magnifying-glass.png"></div></div>';
   return elem
 }
