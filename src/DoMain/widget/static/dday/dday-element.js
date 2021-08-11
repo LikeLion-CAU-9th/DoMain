1a3c451f1106a5bb4a80d016a300fa854f30e0c1
youngkwon02 <jakealden97@gmail.com>
2021-08-11 18:33:17 +0900

    feat : modify widget

--- src/DoMain/widget/static/dday/dday-element.js
+++ src/DoMain/widget/static/dday/dday-element.js
@@ -1,4 +1,4 @@
 const getDdayElements = () => {
-  elem = '<div class="background widget widget-dday" id = "ddaybox" name = "ddaybox"><input type="hidden" class="ddayHidden"><div class="center" id = "ddayFirst"></div></div>';
+  elem = '<div class="background widget widget-dday" id = "ddaybox" name = "ddaybox"><input type="hidden" class="hiddenObj ddayHidden"><div class="center" id = "ddayFirst"></div></div>';
   return elem
 }
--- src/DoMain/widget/static/timer/timer-element.js
+++ src/DoMain/widget/static/timer/timer-element.js
@@ -1,4 +1,4 @@
 const getTimerElements = () => {
-  elem = '<div class="widget widget-timer"><input type="hidden" class="timerHidden"><div class="modify-btn hide" onclick="widgetModalOn()"><img src="/static/image/modify-icon.png"></div><span class="event"></span><span class="remain-time"></span></div>';
+  elem = '<div class="widget widget-timer"><input type="hidden" class="hiddenObj timerHidden"><div class="modify-btn hide"><img src="/static/image/modify-icon.png"></div><span class="event"></span><span class="remain-time"></span></div>';
   return elem
 }
