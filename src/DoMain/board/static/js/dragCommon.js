97da24c664043337e93326c897b62b2a4025b7c0
oereo <dlstpgns0406@gmail.com>
2021-08-12 02:39:49 +0900

    feat: add widget drag function

--- src/DoMain/board/static/js/dragCommon.js
+++ src/DoMain/board/static/js/dragCommon.js
@@ -1,19 +1,19 @@
 const drag = () => {
-  let balls = document.querySelectorAll(".widget");
+  let widgets = document.querySelectorAll(".widget");
   console.log(balls);
 
-  balls.forEach(function(ball, idx) {
+  widgets.forEach(function(widget, idx) {
     // 공의 우선순위 설정
-    let priority = ball.getAttribute("priority");
+    let priority = widget.getAttribute("priority");
     if (!priority) {
       priority = idx + 1;
-      ball.setAttribute("priority", priority);
+      widget.setAttribute("priority", priority);
     }
-    ball.style["z-index"] = priority;
+    widget.style["z-index"] = priority;
 
     // 공 선택 이벤트 바인딩
-    console.log(ball);
-    ball.addEventListener("mousedown", handleMouseDown);
+    // console.log(ball);
+    widget.addEventListener("mousedown", handleMouseDown);
   });
 
   // 마우스 이벤트 바인딩
