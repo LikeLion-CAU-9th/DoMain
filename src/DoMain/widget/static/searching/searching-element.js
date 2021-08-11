164e8a2dc2f753c95d7f68b077d04136d41d5d36
oereo <dlstpgns0406@gmail.com>
2021-08-12 03:34:39 +0900

    feat: add getSearchingElements

--- src/DoMain/widget/static/searching/searching-element.js
+++ src/DoMain/widget/static/searching/searching-element.js
@@ -1,11 +1,5 @@
 const getSearchingElements = () => {
-<<<<<<< HEAD
   elem =
-    '<div class="widget widget-search"><input type="hidden" class="searchingHidden"><div class="modify-btn hide" onclick=""><img src="/static/image/modify-icon.png"></div><input class="searching" placeholder="Search..."></input><div class="searchBtn-container"><img src="/static/image/magnifying-glass.png"></div></div>';
+    '<div class="widget widget-search"><input type="hidden" class="hiddenObj searchingHidden"><div class="modify-btn hide"><img src="/static/image/modify-icon.png"></div><input class="searching" placeholder="Search..."></input><div class="searchBtn-container"><img src="/static/image/magnifying-glass.png"></div></div>';
   return elem;
 };
-=======
-  elem = '<div class="widget widget-search"><input type="hidden" class="hiddenObj searchingHidden"><div class="modify-btn hide"><img src="/static/image/modify-icon.png"></div><input class="searching" placeholder="Search..."></input><div class="searchBtn-container"><img src="/static/image/magnifying-glass.png"></div></div>';
-  return elem
-}
->>>>>>> 807b5a9f2fb4e9860aff3e824abd93d70a9782bf
