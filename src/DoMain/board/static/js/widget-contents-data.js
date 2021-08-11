ccd8d327310697ff5c4dfcd48818974939bf4019
yunseonyeong <64634970+yunseonyeong@users.noreply.github.com>
2021-08-12 06:09:12 +0900

    refactor : edit widget_contents title

--- src/DoMain/board/static/js/widget-contents-data.js
+++ src/DoMain/board/static/js/widget-contents-data.js
@@ -1,7 +1,7 @@
 const WIDGET_CONTENTS = {
   'finance': [{'종목': 'items'}],
   'stickynote': [{'가로': 'width'},{'세로': 'height'},{'제목': 'title'},{'내용':'memo'}],
-  'searching': [{'가로': 'width'},{'세로': 'height'},{'색상': 'bgColor'},{'검색엔진':'engine'}],
-  'timer': [{'반복되는 일정': 'event'},{'시간': 'time'},{'색상': 'bgColor'},{'글씨 크기':'fontSize'}],
+  'searching': [{'가로': 'width'},{'세로': 'height'},{'검색엔진':'engine'}],
+  'timer': [{'제목': 'event'},{'시간': 'time'},{'글씨 크기':'fontSize'}],
   'dday': [{'가로': 'width'},{'세로': 'hight'},{'날짜': 'items'}]
 }
