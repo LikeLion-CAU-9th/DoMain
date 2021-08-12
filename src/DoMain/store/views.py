6166483e3497f1135248369b309582a17a82ad38
oereo <dlstpgns0406@gmail.com>
2021-08-13 05:33:06 +0900

    refactor: fix reply comment

--- src/DoMain/store/views.py
+++ src/DoMain/store/views.py
@@ -109,11 +109,28 @@ def reply_comment(request):
         comment = Comment.objects.get(id=comment_id)
         reply_comments = Reply.objects.filter(comment=comment)
         json_replys = list(reply_comments.values())
-        ret = {
-            "reply_comments": list(reply_comments.values())
+        
+        reply_user_name_data = []
+
+        for user_id in json_replys:
+            reply_user = User_info.objects.get(user_seq=user_id['writer_id']) 
+            reply_user_name_data.append(reply_user.user_name)
+        
+        reply_data=[]
+
+        for comment_id in json_replys:
+            reply = Reply.objects.get(id=comment_id['id']) 
+            reply_data.append(reply.content)      
+
+        print(reply_user_name_data)
+
+        ret = {            
+            "reply_user_name": reply_user_name_data,
+            # "reply_comments": list(reply_comments.values()),
+            "reply_data":reply_data
         }
-        # return HttpResponse(json.dumps(ret), content_type="application/json")
-        return JsonResponse(json_replys, safe=False)
+        return HttpResponse(json.dumps(ret), content_type="application/json")
+        # return JsonResponse(json_replys, safe=False)
 
       
 def like(request):
